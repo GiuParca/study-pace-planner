7b7f6588493bd45cf13ed58b0917641cf815f845
Giulia Parca <124640350+GiuParca@users.noreply.github.com>
2024-05-06 11:37:53 +0100

    adjusting timer settings

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,7 +3,7 @@ import Clock from './Clock';
 import style from './Timer.module.scss';
 import { timeToSeconds } from '../../common/utils/time';
 import { ITask } from '../../types/tasks';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 interface Props{
     selected: ITask | undefined
@@ -11,15 +11,17 @@ interface Props{
 
 function Timer({selected}:Props) {
     const [time, setTime] = useState<number>();
-    if(selected?.time){
-        setTime(timeToSeconds(selected.time));
-    }
+
+    useEffect(() => {
+     if(selected?.time){
+        setTime(timeToSeconds(selected.time))
+     }
+    }, [selected])
     return (
         <div className={style.timer}>
             <p className={style.title}>Choose a card, start the timer</p>
-            Time:{time}
             <div className={style.clockWrapper}>
-                <Clock />
+                <Clock time={time}/>
             </div>
             <Button>
                 Start
